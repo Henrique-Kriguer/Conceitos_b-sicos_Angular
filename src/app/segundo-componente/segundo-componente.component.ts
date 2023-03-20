9cfa85536931a523931ab11f486f3ac4f2aee78a
Henrique Kriguer <henriquekriguer@gmail.com>
2023-03-20 09:19:48 -0300

    Revisando conceitos Angular

---
+++ src/app/segundo-componente/segundo-componente.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-segundo-componente',
+  templateUrl: './segundo-componente.component.html',
+  styleUrls: ['./segundo-componente.component.css']
+})
+export class SegundoComponenteComponent {
+
+    nome = "Henrique Kriguer";
+    dataNascimento = "1971-10-23"
+
+}
